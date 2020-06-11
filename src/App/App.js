1930627ebcbdca95fe6213ab6d26c1d557a2fca0
Joey Petrone <joey.petrone@gmail.com>
2020-06-10 21:50:41 -0500

    wip, map is displaying with marker and popup

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,24 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
+/* eslint-disable quote-props */
 import React from 'react';
-
-import './App.scss';
 import LeafletMap from '../components/LeafletMap/LeafletMap';
 
 class App extends React.Component {
+  state = {
+    foundPin: [],
+  }
+
+  componentDidMount() {
+    const foundPin = [36.159839, -86.777971];
+
+    this.setState({ foundPin });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div id="map-container"></div>
-        <LeafletMap />
-      </div>
+      <LeafletMap foundPin={this.state.foundPin}/>
     );
   }
 }
--- src/components/LeafletMap/LeafletMap.js
+++ src/components/LeafletMap/LeafletMap.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {
   Map, TileLayer, Marker, Popup,
 } from 'react-leaflet';
 
 class LeafletMap extends React.Component {
-    state = {
-      lat: 36.159839,
-      lng: -86.777971,
-      zoom: 20,
-    };
-
-  map = (
-    <Map center={[this.state.lat, this.state.lng]} zoom={this.state.zoom}>
-      <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
-      <Marker position={[this.state.lat, this.state.lng]}>
-        <Popup>
-          A pretty CSS3 popup. <br/> Easily customizable.
-        </Popup>
-      </Marker>
-    </Map>
-  )
+  state = {
+    lat: 36.159839,
+    lng: -86.777971,
+    zoom: 20,
+  };
 
   render() {
     return (
       <div className="LeafletMap">
-        {console.log('this.map: ', this.map)}
-        {ReactDOM.render(this.map, document.getElementById('map-container'))}
+          {
+          this.props.foundPin
+            ? <Map
+                 center={[this.state.lat, this.state.lng]}
+                 zoom={this.state.zoom}
+                 style={{ width: '100%', height: '900px' }}
+              >
+              <TileLayer
+                attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               />
+              <Marker position={this.props.foundPin}>
+                <Popup>
+                  <span>ADDRESS: Loading...</span>
+                  <br/>
+                  <span>BATTALION: <span role="img" aria-label="">🤷‍♀️</span></span><br/>
+                </Popup>
+              </Marker>
+             </Map>
+            : 'Data is loading...'
+          }
       </div>
     );
   }
